aa3e52631e9ccdeb3e86989614cf1292185b371e
mdsulemanr <42205023+mdsulemanr@users.noreply.github.com>
2018-08-08 15:30:54 +0500

    Add files via upload

---
+++ suduku_checker.py
@@ -0,0 +1,112 @@
+# THREE GOLD STARS
+
+# Sudoku [http://en.wikipedia.org/wiki/Sudoku]
+# is a logic puzzle where a game
+# is defined by a partially filled
+# 9 x 9 square of digits where each square
+# contains one of the digits 1,2,3,4,5,6,7,8,9.
+# For this question we will generalize
+# and simplify the game.
+
+# Define a procedure, check_sudoku,
+# that takes as input a square list
+# of lists representing an n x n
+# sudoku puzzle solution and returns the boolean
+# True if the input is a valid
+# sudoku square and returns the boolean False
+# otherwise.
+
+# A valid sudoku square satisfies these
+# two properties:
+
+#   1. Each column of the square contains
+#       each of the whole numbers from 1 to n exactly once.
+
+#   2. Each row of the square contains each
+#       of the whole numbers from 1 to n exactly once.
+
+# You may assume the the input is square and contains at
+# least one row and column.
+
+correct = [[1, 2, 3],
+           [2, 3, 1],
+           [3, 1, 2]]
+
+incorrect = [[1, 2, 3, 4],
+             [2, 3, 1, 3],
+             [3, 1, 2, 3],
+             [4, 4, 4, 4]]
+
+incorrect2 = [[1, 2, 3, 4],
+              [2, 3, 1, 4],
+              [4, 1, 2, 3],
+              [3, 4, 1, 2]]
+
+incorrect3 = [[1, 2, 3, 4, 5],
+              [2, 3, 1, 5, 6],
+              [4, 5, 2, 1, 3],
+              [3, 4, 5, 2, 1],
+              [5, 6, 4, 3, 2]]
+
+incorrect4 = [['a', 'b', 'c'],
+              ['b', 'c', 'a'],
+              ['c', 'a', 'b']]
+
+incorrect5 = [[1, 1.5],
+              [1.5, 1]]
+
+matrix6 = [[1, 1.5, 3], [3, 1, 1.5], [1.5, 3, 1]]
+
+
+def check_sudoku(matrix):
+    if matrix==[]:
+        return True
+    n=len(matrix)
+    for i in range(0, n):
+        if n!=len(matrix[i]):
+            return False
+    number=1
+    while number <=n:
+        i=0
+        while i<n:
+            j=0
+            row_check=0
+            col_check=0
+            while j<n:
+                if matrix[i][j]==number:
+                    row_check+=1
+                if matrix[j][i]==number:
+                    col_check+=1
+                j+=1
+            if row_check!=1 or col_check!=1:
+                return False
+            else:
+                i+=1
+        number += 1
+
+    return True
+
+
+
+
+
+print(check_sudoku(incorrect))
+# >>> False
+
+print(check_sudoku(correct))
+# >>> True
+
+print(check_sudoku(incorrect2))
+# >>> False
+
+print(check_sudoku(incorrect3))
+# >>> False
+
+print(check_sudoku(incorrect4))
+# >>> False
+
+print(check_sudoku(incorrect5))
+# >>> False
+
+print(check_sudoku(matrix6))
+# >>> False
