c1d08a19132f8018d78e410145f8cc41d3ddb208
sulemanrafi <suleman.rafi@arbisoft.com>
2018-10-16 18:45:52 +0500

    new changes

---
+++ average_numbers.py
@@ -0,0 +1,25 @@
+def ave():
+    total = 0
+    count = 0
+    while True:
+        try:
+            inp = input('Enter a number: ')
+            if inp in ['Done', 'done', 'DONE', '/n']:
+                if total==0:
+                    return
+                else:
+                    break
+            value = float(inp)
+            total = total + value
+            count = count + 1
+            average = total / count
+        except ValueError:
+            print('Enter only numbers')
+            continue
+        except ZeroDivisionError:
+            return 0
+    return ('{}: {}'.format('Average', average))
+
+print(ave())
+
+
