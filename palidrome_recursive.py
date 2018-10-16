10bcb55092d8663ffb46a8d9f023f586b974bf7b
sulemanrafi <suleman.rafi@arbisoft.com>
2018-10-16 18:50:40 +0500

    changed

--- palidrome_recursive.py
+++ palidrome_recursive.py
@@ -8,11 +8,10 @@
 def is_palindrome(s):
     if s=='':
         return True
-    if len(s)%2!=0:
-        return False
+    s=s.lower()
     i=0
     j=-1
-    while i < len(s)/2:
+    while i < len(s):
         if s[i]!=s[j]:
             return False
         i+=1
@@ -20,10 +19,73 @@ def is_palindrome(s):
     return True
 
 
+def is_palindrome(s):
+    if s == '':
+        return True
+    s=s.lower()
+    for i, j in zip(s, reversed(s)):
+        if i!=j:
+            return False
+    return True
+
+
+def is_palindrome(s):
+    if s == '':
+        return True
+    s=s.lower()
+    reverse_s=reversed(s)
+    if list(s)!=list(reverse_s):
+        return False
+    return True
+
+def is_palindrome(s):
+    if s == '':
+        return True
+    s=s.lower()
+    j=''
+    j=j.join(reversed(s))
+    if s!=j:
+        return False
+    return True
+
+def is_palindrome(s):
+    if s == '':
+        return True
+    s=s.lower()
+    if s!=s[::-1]:
+        return False
+    return True
+
+def is_palindrome(s):
+    if s == '':
+        return True
+    s=s.lower()
+    i=0
+    j=-1
+    while i < len(s):
+        if s[i]!=s[j]:
+            return False
+        else:
+            i+=1
+            j-=1
+
+    return True
+
+def is_palindrome(s):
+    if len(s) < 2:
+        return True
+    s=s.lower()
+    if s[0]!=s[-1]:
+        return False
+    else:
+        is_palindrome(s[1:-1])
+    return True
 
 print(is_palindrome(''))
 # >>> True
 print(is_palindrome('abab'))
 # >>> False
 print(is_palindrome('abba'))
+# >>> True
+print(is_palindrome('kayaK'))
 # >>> True
